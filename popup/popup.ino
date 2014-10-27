cb9f1240086f406ebabc8753994616e30b145464
jw26 <james.x.wilson@gmail.com>
2014-10-27 19:57:40 +0000

    sleep and wake. need to work on the circuit to minimise current leak.

--- popup/popup.ino
+++ popup/popup.ino
@@ -4,10 +4,10 @@
 
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/sleep.h>
 
 volatile int state = LOW;
 volatile uint8_t pulse;
-volatile uint8_t moving;
 
 void setup()
 {
@@ -18,14 +18,21 @@ void setup()
   TIMSK0 |= (1<<OCIE0A);    // Turn on the compare interrupt
   TCCR0B |= (1<<CS00); // no prescaler
   TCCR0A |= (1<<WGM01);     // CTC
-  OCR0A = 119;//95; // (0.1/((1/(9600))))-1 think this is wrong. forgot about clk/8 i think
-              // should be (0.1/(1/(9600/8)))-1 = 119.. might that explain the drift on 20ms?
+  OCR0A = 119; // should be (0.1/(1/(9600/8)))-1 = 119
 
   // knock interrupt
   DDRB &= ~(1<<PB1);
   PORTB |= (1<<PB1); // enable the pull-up
-  GIMSK = 1<<INT0; // interrupt on pb1
-  MCUCR = (1<<ISC01) | (0<<ISC00); // falling edge
+  GIMSK |= (1<<INT0); // interrupt on pb1
+  ADCSRA &= ~(1<<ADEN); // adc off
+  ACSR = (1<<ACD); // comparitor off
+  //MCUCR = (1<<ISC01) | (0<<ISC00); // falling edge
+
+  // pb2 to power switch servo
+  DDRB |= (1<<PB2);
+  PORTB &= ~(1<<PB2); // servo off
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
   sei();
 
@@ -41,12 +48,22 @@ void setup()
 
 }
 
+void sleep_now() {
+  //sleep_enable();
+  GIMSK |= (1<<INT0); // interrupt on pb1
+  sleep_mode(); // do it
+  GIMSK &= ~(1<<INT0); // disable interrupt on pb1
+  sleep_disable(); // wake up
+}
+
 void loop() {
-  _delay_ms(2000);
-  //if (state == HIGH) {
+  //_delay_ms(2000);
+  if (state == HIGH) {
     PORTB |= (1<<PB4); // lights on
     PORTB |= (1<<PB0); // lights on
     _delay_ms(500);
+
+    PORTB |= (1<<PB2); // servo on
 
     // 2 stage on the way up
     DDRB |= (1<<PB3);
@@ -58,7 +75,7 @@ void loop() {
 
     DDRB |= (1<<PB3);
     for(; pulse>2; pulse--) {
-      _delay_ms(60);
+      _delay_ms(30);
     }
     DDRB &= ~(1<<PB3);
 
@@ -66,12 +83,15 @@ void loop() {
     PORTB &= ~(1<<PB4);
     PORTB &= ~(1<<PB0);
 
+    PORTB &= ~(1<<PB2); // servo off
+
     state = LOW;
-  //}
+    sleep_now();
+  }
 }
 
 ISR(INT0_vect) {
-  _delay_ms(240); // pulse is ~230ms
+  //_delay_ms(240); // pulse is ~230ms
   state = HIGH;
 }
 
@@ -80,7 +100,7 @@ ISR(TIM0_COMPA_vect) {
   static unsigned int tick = 0;
 
   // tick is about 0.1ms
-  if (tick >= 400) { // every 20ms is 250?!?!?, must be loosing cycles somewhere
+  if (tick >= 400) { // 40ms window seems to work mostly
     tick = 0;
   }
 
